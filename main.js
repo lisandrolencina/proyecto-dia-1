bd074dc59e4030090d0ff8f4a0cb2827be8c13e9
lisandrolencina <lisandrolencina.99@gmail.com>
2022-01-20 19:34:22 -0300

    inicio de la web

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-cards', { delay: 500 });
+  ScrollReveal().reveal('.new-cards-2', { delay: 500 });
+  ScrollReveal().reveal('.form', { delay: 500 });
